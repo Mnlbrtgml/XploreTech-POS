2ce0b9ab670d3d28d38f7fbff373a5b298d40853
MarkDennis1 <markdennissanchez1@gmail.com>
2022-12-16 19:46:47 +0800

    Add Reports

--- XploreTech-POS/Forms/frmFilterReport.vb
+++ XploreTech-POS/Forms/frmFilterReport.vb
@@ -1,5 +1,10 @@
 ﻿Public Class frmFilterReport
     Private Sub frmFilterReport_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         formShadow.SetShadowForm(Me)
+
+    End Sub
+
+    Private Sub Guna2Button1_Click(sender As Object, e As EventArgs) Handles Guna2Button1.Click
+        Me.Close()
     End Sub
 End Class
--- XploreTech-POS/Forms/frmReports.vb
+++ XploreTech-POS/Forms/frmReports.vb
@@ -1,6 +1,37 @@
 ﻿Public Class frmReports
     Private Sub frmReports_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
         Me.report.RefreshReport()
+        cmbxReport.Items.Clear()
+        cmbxReport.Items.Add("Milktea Report")
+        cmbxReport.Items.Add("Additional Products Report")
+        cmbxReport.Items.Add("Addons Report")
+        cmbxReport.Items.Add("User Report")
+        cmbxReport.Items.Add("Transactions Report")
+        cmbxReport.Items.Add("Activity Log Report")
+        cmbxReport.SelectedIndex = 0
+
+    End Sub
+
+    Private Sub btnFilter_Click(sender As Object, e As EventArgs) Handles btnFilter.Click
+        Dim newFrmFilter = New frmFilterReport
+        newFrmFilter.ShowDialog()
+
+    End Sub
+
+    Private Sub cmbxReport_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbxReport.SelectedIndexChanged
+        Select Case cmbxReport.SelectedItem
+            Case "Milktea Report"
+                LoadReport("MilkteaProducts")
+            Case "Additional Products Report"
+                LoadReport("OtherProducts")
+            Case "Addons Report"
+                LoadReport("Toppings")
+            Case "User Report"
+                LoadReport("UserAccounts")
+            Case "Transactions Report"
+                LoadReport("Transaction")
+            Case "Activity Log Report"
+                LoadReport("ActivityLog")
+        End Select
     End Sub
 End Class
